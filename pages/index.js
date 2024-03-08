d2bf6acde318734f2479526d0b60b42532c6f97f
Siddhant Pathak <76740501+siddhant1309@users.noreply.github.com>
2024-03-08 16:26:07 +0530

    Update index.js
    try adding the Google Analytics Code Snippet, to know the amount of people visiting my page

--- pages/index.js
+++ pages/index.js
@@ -53,6 +53,16 @@ export default function Home() {
       {data.showCursor && <Cursor />}
       <Head>
         <title>{data.name}</title>
+        {/* Google Analytics Tracking Code */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-62WSHFDGKQ"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-62WSHFDGKQ');
+          `
+        }} />
       </Head>
 
       <div className="gradient-circle"></div>
